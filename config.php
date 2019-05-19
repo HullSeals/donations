ac6b7a94b4ea2b625515ff8d571ce6e61cb049a8
David Sangrey <davidsangrey@gmail.com>
2019-05-19 01:12:41 +0000

    Update config.php

--- config.php
+++ config.php
@@ -2,8 +2,8 @@
 require_once('vendor/autoload.php');
 
 $stripe = [
-  "secret_key"      => "sk_live_BV1kYNf3SafXzZtJy5tv4KI800Jh9PyMcX",
-  "publishable_key" => "pk_live_KiZ55OXEwCXcsyvKRoszm0mw00AJM6XE4j",
+  "secret_key"      => "",
+  "publishable_key" => "",
 ];
 
 \Stripe\Stripe::setApiKey($stripe['secret_key']);
