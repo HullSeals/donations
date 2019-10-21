305da75c81cd97405870302619f1b85f022d827b
Rixxan <rixxan@hullseals.space>
2019-10-21 16:32:22 -0400

    Update Linkage

--- index.php
+++ index.php
@@ -26,7 +26,7 @@
     <div id="home">
       <header>
           <nav class="navbar container navbar-expand-lg navbar-expand-md navbar-dark" role="navigation">
-              <a class="navbar-brand" href="../index.html"><img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo"> Hull Seals</a>
+              <a class="navbar-brand" href="../"><img src="../images/emblem_scaled.png" height="30" class="d-inline-block align-top" alt="Logo"> Hull Seals</a>
 
               <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                   <span class="navbar-toggler-icon"></span>
@@ -35,7 +35,7 @@
               <div class="collapse navbar-collapse" id="navbarNav">
                   <ul class="navbar-nav">
                       <li class="nav-item">
-                          <a class="nav-link" href="../index.html">Home</a>
+                          <a class="nav-link" href="../">Home</a>
                       </li>
                       <li class="nav-item">
                           <a class="nav-link" href="../knowledge">Knowledge Base</a>
@@ -50,7 +50,7 @@
                           <a class="nav-link" href="../contact">Contact</a>
                       </li>
                       <li class="nav-item">
-                          <a class="nav-link" href="#">Login/Register</a>
+                          <a class="nav-link" href="https://auth.hullseals.space/">Login/Register</a>
                       </li>
                   </ul>
               </div>
